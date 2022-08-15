5ab5f70c243c5599e2c1264ec5c76592491694db
Kartik Barmaiya <kartik.barmaiya28@gmail.com>
2022-08-15 20:31:43 +0530

    Updated Sign up

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+//DO copy the code from index.css
 
 const InputField = ({type,placeholder}) => {
   return (
--- src/components/login/SignupOne.jsx
+++ src/components/login/SignupOne.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import InputField from '../InputField'
+import { ChevronLeftIcon } from '@heroicons/react/outline'
 
 const SignupOne = () => {
   return (
     <div className='w-[50rem] flex flex-col items-center'>
 
       <div className='w-full flex px-10 my-8 justify-between'>
-        <a href='/'> <p>back</p> </a>
+        <a href='/' className='flex flex-row'>
+          <ChevronLeftIcon className='mr-1 h-5 mt-1'/>
+          <p>back</p>
+        </a>
         <p>Step 01/03</p>
       </div>
 
@@ -18,6 +22,16 @@ const SignupOne = () => {
       <div className='my-2'>
         <p className='text-gray-700 mb-3'>Company website</p>
         <InputField type='text' placeholder="Enter website URL" />
+      </div>
+
+      <div className='my-2'>
+        <p className='text-gray-700 mb-3'>Company mail</p>
+        <InputField type='email' placeholder="Enter E-mail" />
+      </div>
+
+      <div className='my-2'>
+        <p className='text-gray-700 mb-3'>Description</p>
+        <InputField type='email' placeholder="Enter description" />
       </div>
 
     </div>
