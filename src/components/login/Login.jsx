3be35f399e158e9132806c1d288fcac844d1ace3
Kartik Barmaiya <kartik.barmaiya28@gmail.com>
2022-08-15 20:54:45 +0530

    Login Color updated

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -15,8 +15,8 @@ const Login = () => {
         <div className='w-full md:w-3/5 p-5'>  {/* Login Section */}
         {/* <img src={logo} alt='logo' className='h-10' /> */}
         <div className='py-10'>
-            <h2 className='text-3xl font-bold text-blue-400 mb-2'>Sign in to account</h2>
-            <div className='border-4 w-10 border-blue-400 inline-block mb-2' />
+            <h2 className='text-3xl font-bold text-[#003979] mb-2'>Sign in to account</h2>
+            <div className='border-4 w-10 border-[#003979] inline-block mb-2' />
             {/*Social Login Section below */}
             <div className='flex justify-center my-2'>
                 <a href='social' className='border-2 border-gray-200 rounded-full p-3 mx-1 hover:bg-gray-400 hover:text-white'>
@@ -45,13 +45,13 @@ const Login = () => {
                     <label className='flex items-center text-sm'><input type='checkbox' name='remember' className='mr-1'/>Remember me</label>
                     <a href='forgot-password' className='text-xs'>Forgot Password?</a>
                 </div>
-                <a href='signin' className='border-2 border-blue-400 text-blue-400 font-semibold rounded-full px-12 py-2 inline-block hover:bg-blue-400 hover:text-white'>SignIn</a>
+                <a href='signin' className='border-2 border-[#003979] text-[#003979] font-semibold rounded-full px-12 py-2 inline-block hover:bg-[#003979] hover:text-white'>SignIn</a>
             </div>
 
 
         </div>
         </div>
-        <div className='w-full md:w-2/5 md:h-[492px] bg-blue-400 text-white px-12 py-36'>  {/* signup Section */}
+        <div className='w-full md:w-2/5 md:h-[492px] bg-[#003979] text-white px-12 py-36'>  {/* signup Section */}
             <h2 className='text-3xl font-bold mb-2'>Hello, company!</h2>
             <div className='border-4 w-10 border-white inline-block mb-2' />
             <div className='90%'>
@@ -59,7 +59,7 @@ const Login = () => {
               <p className='text-xl mb-6'>Hire our trained and skilled WEB3 developers</p>
               <p className='mb-10'>Fill up company info and start journey with us!</p>
             </div>
-            <a href='signup' className='border-2 border-white font-semibold rounded-full px-12 py-2 inline-block hover:bg-white hover:text-blue-400'>SignUp</a>
+            <a href='signup' className='border-2 border-white font-semibold rounded-full px-12 py-2 inline-block hover:bg-white hover:text-[#003979]'>SignUp</a>
         </div>
 
 
--- src/components/login/Signup.jsx
+++ src/components/login/Signup.jsx
@@ -7,7 +7,7 @@ import SignupTwo from './SignupTwo'
 const Signup = () => {
   return (
     <div className='bg-gray-200 flex flex-col items-center justify-center py-6'>
-        <h1 className='text-3xl font-bold text-blue-400 mb-2'>New business registration</h1>
+        <h1 className='text-3xl font-bold text-[#003979] mb-2'>New business registration</h1>
         <div className='bg-white'>
           <Switch>
               <SignupOne />
