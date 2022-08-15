dc7445f026e22d8a70e5c3de103a40542f9e189c
Kartik Barmaiya <kartik.barmaiya28@gmail.com>
2022-08-15 21:10:04 +0530

    Dashboard added

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Header from './header/Header'
+import Navbar from './Navbar/Navbar'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div>
+      <Header />
+      <div className="w-full min-h-[90vh] grid grid-cols-12">
+        <Navbar/>
+        <div>
+          {/* Left part */}
+          {/* Right part */}
+        </div>
+      </div>
+    </div>
   )
 }
 
